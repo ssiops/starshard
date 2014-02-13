ac2b3c9f717e0e22e124e53730fdba642b12d966
akasfei <akasfei@gmail.com>
2014-02-13 20:49:38 +0800

    Floatmsg as $.alert()

--- static/js/application/application.js
+++ static/js/application/application.js
@@ -1,25 +1,30 @@
 !function ($) {
 
-  $(function(){
-    $.alert = function(opt) {
-      var self = $('.float-msg');
-      var alert_class = 'alert alert-danger';
-      var msg = 'An error has occurred.';
-      if (typeof opt === 'string'){
-        if (opt === 'show')
-          return self.addClass('active');
-        if (opt === 'hide')
-          return self.removeClass('active');
-      }
-      else if (typeof opt.msg === 'undefined')
-        return self;
-      if (typeof opt.style === 'undefined')
-        alert_style = 'alert alert-danger';
-      else
-        alert_style = 'alert alert-' + opt.style;
-      msg = opt.msg;
-      self.html('<div class="' + alert_style + '"><p>' + msg + '</p></div>').floatmsg('show');
-      return self;
+  $.alert = function(opt) {
+    var self = $('.float-msg');
+    var alert_class = 'alert alert-danger';
+    var msg = 'An error has occurred.';
+    if (typeof opt === 'string'){
+      if (opt === 'show')
+        return self.addClass('active');
+      if (opt === 'hide')
+        return self.removeClass('active');
     }
+    else if (typeof opt.msg === 'undefined')
+      return self;
+    if (typeof opt.style === 'undefined')
+      alert_style = 'alert alert-danger';
+    else
+      alert_style = 'alert alert-' + opt.style;
+    msg = opt.msg;
+    self.html('<div class="' + alert_style + '"><p>' + msg + ' <a href="#" class="float-msg-dismiss">Dismiss</a></p></div>');
+    $.alert('show');
+    return self;
+  }
+
+  $(function(){
+    $('.float-msg').on('click', '.float-msg-dismiss', function (e) {
+      $.alert('hide');
+    });
   });
 }(window.jQuery)
