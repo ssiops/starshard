a6d4c011918bef11f770e49be07108ff60729b67
akasfei <akasfei@gmail.com>
2014-04-06 11:01:40 +0800

    Operations page
    Set/unset admin
    Uemove user
    Log filter

--- static/js/application/starshard-ops.js
+++ static/js/application/starshard-ops.js
@@ -1,13 +1,18 @@
 !function ($) {
   var update_logs = function () {
+    var q = $('.query-logs').val();
+    var query = {}
+    if (q.length > 0)
+      query.q = q;
     $.ajax({
       url: '/ops/logs',
       method: 'GET',
+      data: query,
       dataType: 'json',
       success: function (data) {
         if (data && typeof data.html !== 'undefined') {
           $('.logs-list').html(data.html);
-        } else if (typeof data.err !== 'undefined')
+        } else if (data && typeof data.err !== 'undefined')
           console.log(data.err);
       }
     });
@@ -21,7 +26,7 @@
       success: function (data) {
         if (data && typeof data.html !== 'undefined') {
           $('.users-list').html(data.html);
-        } else if (typeof data.err !== 'undefined')
+        } else if (data && typeof data.err !== 'undefined')
           console.log(data.err);
       }
     });
@@ -37,6 +42,57 @@
 
     $('.refresh-users').on('click', function (e) {
       update_users();
+    });
+
+    $('.users-list').on('click', '.set-admin', function (e) {
+      e.preventDefault();
+      var self = $(this);
+      $.ajax({
+        url: '/ops/users/admin',
+        method: 'PUT',
+        data: {name: self.parents('li.dropdown').attr('data-name')},
+        success: function (data) {
+          if (data && typeof data.msg !== 'undefined') {
+            $.alert({msg: data.msg, style: 'success'});
+            update_users();
+          } else if (data && typeof data.err !== 'undefined')
+            console.log(data.err);
+        }
+      })
+    });
+
+    $('.users-list').on('click', '.unset-admin', function (e) {
+      e.preventDefault();
+      var self = $(this);
+      $.ajax({
+        url: '/ops/users/admin',
+        method: 'DELETE',
+        data: {name: self.parents('li.dropdown').attr('data-name')},
+        success: function (data) {
+          if (data && typeof data.msg !== 'undefined') {
+            $.alert({msg: data.msg, style: 'success'});
+            update_users();
+          } else if (data && typeof data.err !== 'undefined')
+            console.log(data.err);
+        }
+      })
+    });
+
+    $('.users-list').on('click', '.remove-user', function (e) {
+      e.preventDefault();
+      var self = $(this);
+      $.ajax({
+        url: '/ops/users',
+        method: 'DELETE',
+        data: {name: self.parents('li.dropdown').attr('data-name')},
+        success: function (data) {
+          if (data && typeof data.msg !== 'undefined') {
+            $.alert({msg: data.msg, style: 'success'});
+            update_users();
+          } else if (data && typeof data.err !== 'undefined')
+            console.log(data.err);
+        }
+      })
     });
   });
 }(window.jQuery)
