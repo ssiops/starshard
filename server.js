964bcc07f31910b4627b28c9f0b07e39dbc4ac50
akasfei <akasfei@gmail.com>
2014-04-21 17:19:15 +0800

    Express 4.0

--- server.js
+++ server.js
@@ -10,10 +10,22 @@ var async = require('async');
 var cluster = require('cluster');
 var express = require('express');
 var redis = require('redis');
-var redisClient = redis.createClient();
-var RedisStore = require('connect-redis')(express);
-var init = require('./lib/init.js');
 
+var logger = require('morgan');
+var compress = require('compression');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var serveStatic = require('serve-static');
+var methodOverride = require('method-override');
+var connectRedis = require('connect-redis');
+var multiparty = require('connect-multiparty');
+var errorHandler = require('errorhandler');
+
+var redisClient = redis.createClient();
+var RedisStore = require('connect-redis')(session);
+
+var init = require('./lib/init.js');
 var view = require('./lib/view.js');
 var error = require('./lib/error.js');
 
@@ -27,37 +39,32 @@ if (cluster.isMaster) {
     console.log('worker ' + worker.process.pid + ' died');
   });
 } else {
+  var server = module.exports = express();
 
-var server = module.exports = express();
-
-server.configure(function () {
   server.set('env', pkg.production == true ? 'production': 'development');
   if (pkg.production !== true)
-    server.use(express.logger('dev'));
-  server.use(express.compress());
-  server.use(express.json());
-  server.use(express.urlencoded());
-  server.use(require('connect-multiparty')({limit: '8mb'}));
-  server.use(express.cookieParser());
-  server.use(express.session({store: new RedisStore({client: redisClient}), secret: 'SSIv4'}));
-  server.use(express.static(__dirname + '/_static'));
-  server.use(express.static(__dirname + '/usercontent'));
-  server.use(express.methodOverride());
-  server.use(server.router);
-  server.use(error.s404);
-  server.use(error.s500);
+    server.use(logger('dev'));
+  server.use(compress());
+  server.use(bodyParser());
+  server.use(multiparty({limit: '8mb'}));
+  server.use(cookieParser());
+  server.use(session({store: new RedisStore({client: redisClient}), secret: 'SSIv4'}));
+  server.use(serveStatic(__dirname + '/_static'));
+  server.use(serveStatic(__dirname + '/usercontent'));
+  server.use(methodOverride());
 
   server.engine('html', view.__express);
   server.set('view engine', 'html');
   server.set('views', __dirname + '/views');
 
-  server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+  server.use(errorHandler({ dumpExceptions: true, showStack: true }));
 
-init(server, function (err) {
-  assert.equal(null, err);
-  server.listen(80, function(){
-    console.log("Cluster #%d booted in %d ms. [@port %d in %s mode]", cluster.worker.id, new Date().getTime() - t.getTime(), 80, server.settings.env);
+  init(server, function (err) {
+    assert.equal(null, err);
+    server.use(error.s404);
+    server.use(error.s500);
+    server.listen(80, function(){
+      console.log('Cluster #%d booted in %d ms. [@port %d in %s mode]', cluster.worker.id, new Date().getTime() - t.getTime(), 80, server.settings.env);
+    });
   });
-});
 }
